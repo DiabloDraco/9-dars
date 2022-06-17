1d7d69d4414b3ab5bd51b88ea0958070a5231414
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-06-17 13:33:39 +0500

    add

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ var elBtn = document.querySelector(".header__button");
 elBtn.addEventListener("click", ( )=> {
     elHeader.classList.toggle("header__container--active")
 })
+
+var elForm = document.querySelector(".drink__right-list")
+
+elForm.addEventListener("click", function(evt) {
+  var clickedItem = evt.target.classList;
+
+  if (clickedItem == "drink__list-arrow") {
+    var parentClass = evt.target.parentNode.closest(".drink__form-item")
+
+    parentClass.classList.toggle("drink__r-item--open")
+  }
+})
